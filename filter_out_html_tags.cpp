3e45ef6c6f787fbc1a7e84b7c2308c640be6091c
Conor Hoekstra <codereport@outlook.com>
2023-05-10 12:48:20 -0400

    example 3 working now

--- filter_out_html_tags.cpp
+++ filter_out_html_tags.cpp
@@ -38,23 +38,15 @@ auto filter_out_html_tags(std::string_view sv) {
         |> transform($, [](auto t) { return std::get<1>(t); });
 }
 
-// auto filter_out_html_tags2(std::string_view sv) {
-//     return sv 
-//         |> transform($, [](auto e) { return e == '<' or e == '>'; }) 
-//         |> zip_transform(std::logical_or{}, $, transform($, [first = true, eq = false](auto e) mutable {
-//             if (first) {
-//                 eq = e;
-//                 first = false;
-//             } else {
-//                 eq = eq != e;
-//             }
-//             return eq;
-//         }))
-//         |> zip($, sv)
-//         |> filter($, [](auto t) { return not std::get<0>(t); })
-//         |> transform($, [](auto t) { return std::get<1>(t); })
-//         | ranges::to<std::string>;
-// }
+auto filter_out_html_tags2(std::string_view sv) {
+    return sv 
+        |> transform($, [](auto e) { return e == '<' or e == '>'; }) 
+        |> zip_transform(std::logical_or{}, $, scan_left($, true, std::not_equal_to{}))
+        |> zip($, sv)
+        |> filter($, [](auto t) { return not std::get<0>(t); })
+        |> transform($, [](auto t) { return std::get<1>(t); })
+        | ranges::to<std::string>;
+}
 
 // auto filter_out_html_tags3(std::string_view sv) {
 //     return sv 
@@ -102,7 +94,7 @@ auto filter_out_html_tags4(std::string_view sv) {
 
 int main() {
     fmt::print("{}\n", filter_out_html_tags ("<div>Hello <b>C++North!</b></div>")); // working
-    // fmt::print("{}\n", filter_out_html_tags2("<div>Hello <b>C++North!</b></div>"));
+    fmt::print("{}\n", filter_out_html_tags2("<div>Hello <b>C++North!</b></div>")); // working
     // fmt::print("{}\n", filter_out_html_tags3("<div>Hello <b>C++North!</b></div>"));
     fmt::print("{}\n", filter_out_html_tags4("<div>Hello <b>C++North!</b></div>")); // working
     // fmt::print("{}\n", filter_out_html_tags5("<div>Hello <b>C++North!</b></div>"));
