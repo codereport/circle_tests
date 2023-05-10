7d5661350977ff8f0b5729763ac589ef3258c908
Conor Hoekstra <codereport@outlook.com>
2023-05-10 12:15:34 -0400

    Changes

--- filter_out_html_tags.cpp
+++ filter_out_html_tags.cpp
@@ -2,7 +2,7 @@
 #include <ranges>
 #include <vector>
 
-// #include <range/v3/range/conversion.hpp>
+#include <range/v3/range/conversion.hpp>
 #include <range/v3/view/partial_sum.hpp> 
 
 #include <fmt/core.h>
@@ -19,19 +19,94 @@
 // }
 
 using namespace std::views;
+namespace rv = ranges::views;
+
 
 auto filter_out_html_tags(std::string_view sv) {
     return sv 
         |> transform($, [](auto e) { return e == '<' or e == '>'; }) 
-        |> zip_transform(std::logical_or{}, $, $ |> std::views::reverse($))
+        |> zip_transform(std::logical_or{}, $, transform($, [first = true, eq = false](auto e) mutable {
+            if (first) {
+                eq = e;
+                first = false;
+            } else {
+                eq = eq != e;
+            }
+            return eq;
+        }))
         |> zip($, sv)
         |> filter($, [](auto t) { return not std::get<0>(t); })
         |> transform($, [](auto t) { return std::get<1>(t); });
-        // |> ranges::to<std::string>($);
 }
 
+// auto filter_out_html_tags2(std::string_view sv) {
+//     return sv 
+//         |> transform($, [](auto e) { return e == '<' or e == '>'; }) 
+//         |> zip_transform(std::logical_or{}, $, transform($, [first = true, eq = false](auto e) mutable {
+//             if (first) {
+//                 eq = e;
+//                 first = false;
+//             } else {
+//                 eq = eq != e;
+//             }
+//             return eq;
+//         }))
+//         |> zip($, sv)
+//         |> filter($, [](auto t) { return not std::get<0>(t); })
+//         |> transform($, [](auto t) { return std::get<1>(t); })
+//         | ranges::to<std::string>;
+// }
+
+// auto filter_out_html_tags3(std::string_view sv) {
+//     return sv 
+//         |> transform($, [](auto e) { return e == '<' or e == '>'; }) 
+//         |> zip_transform(std::logical_or{}, $, transform($, [first = true, eq = false](auto e) mutable {
+//             if (first) {
+//                 eq = e;
+//                 first = false;
+//             } else {
+//                 eq = eq != e;
+//             }
+//             return eq;
+//         }))
+//         |> zip($, sv)
+//         |> filter($, [](auto t) { return not std::get<0>(t); })
+//         |> transform($, [](auto t) { return std::get<1>(t); })
+//         |> ranges::to<std::string>($);
+// }
+
+// auto filter_out_html_tags4(std::string_view sv) {
+//     return sv 
+//         |> transform($, [](auto e) { return e == '<' or e == '>'; }) 
+//         |> zip_transform(std::logical_or{}, $, $ |> transform([first = true, eq = false](auto e) mutable {
+//             if (first) {
+//                 eq = e;
+//                 first = false;
+//             } else {
+//                 eq = eq != e;
+//             }
+//             return eq;
+//         }))
+//         |> zip($, sv)
+//         |> filter($, [](auto t) { return not std::get<0>(t); })
+//         |> transform($, [](auto t) { return std::get<1>(t); });
+// }
+
+// auto filter_out_html_tags5(std::string_view sv) {
+//     return sv 
+//         |> transform($, [](auto e) { return e == '<' or e == '>'; }) 
+//         |> zip_transform(std::logical_or{}, $, rv::partial_sum($, std::not_equal_to{}))
+//         |> zip($, sv)
+//         |> filter($, [](auto t) { return not std::get<0>(t); })
+//         |> transform($, [](auto t) { return std::get<1>(t); });
+// }
+
 int main() {
-    fmt::print("{}\n", filter_out_html_tags("<div>Hello <b>C++North!</b></div>"));
+    fmt::print("{}\n", filter_out_html_tags ("<div>Hello <b>C++North!</b></div>"));
+    // fmt::print("{}\n", filter_out_html_tags2("<div>Hello <b>C++North!</b></div>"));
+    // fmt::print("{}\n", filter_out_html_tags3("<div>Hello <b>C++North!</b></div>"));
+    // fmt::print("{}\n", filter_out_html_tags4("<div>Hello <b>C++North!</b></div>"));
+    // fmt::print("{}\n", filter_out_html_tags5("<div>Hello <b>C++North!</b></div>"));
 
     return 0;
 }
